4f558d472f2a1dc2f528ab08b0f99dc50845a8c0
dreamldx <dreamldx@gmail.com>
2025-03-12 20:56:09 -0700

    Create file

---
+++ 8-StringtoInteger(atoi)/8-StringtoInteger(atoi).py
@@ -0,0 +1,44 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        n = len(s)
+
+        leading = True
+        sign = 1
+
+        result = 0
+
+        for i in range(0, n):
+            if leading:
+                if s[i] == ' ':
+                   continue
+                elif s[i] == '-':
+                    sign = -1
+                    leading = False
+                    continue
+                elif s[i] == '+':
+                    leading = False
+                    continue
+                else:
+                    leading = False
+
+            print(f"index: {i} -> {s[i]}")
+
+
+            if s[i].isdigit():
+                result = result * 10 + int(s[i])
+                if  sign * result <  -2147483648:
+                    result = 2147483648
+                    break
+
+                if sign * result >= 2147483648:
+                    result = 2147483647
+                    break
+            else:
+                break
+
+        return sign * result
+
+
+            
+
+             
