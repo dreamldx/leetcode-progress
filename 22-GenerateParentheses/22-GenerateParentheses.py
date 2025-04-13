ee69b128230ab335e09c805be0e7998af7196a3b
dreamldx <dreamldx@gmail.com>
2025-04-12 18:36:05 -0700

    Create file

---
+++ 22-GenerateParentheses/22-GenerateParentheses.py
@@ -0,0 +1,15 @@
+# Last updated: 4/12/2025, 6:36:04 PM
+class Solution:
+     def generateParenthesis(self, n: int) -> List[str]:
+        if n == 0:
+            return [""]
+
+        answer = []
+        for left_count in range(n):
+            for left_string in self.generateParenthesis(left_count):
+                for right_string in self.generateParenthesis(
+                    n - 1 - left_count
+                ):
+                    answer.append("(" + left_string + ")" + right_string)
+
+        return answer
