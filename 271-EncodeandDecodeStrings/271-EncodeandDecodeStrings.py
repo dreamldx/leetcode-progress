f8b6ad715f538601ac1096f8fdeaf833b06525f4
dreamldx <dreamldx@gmail.com>
2025-09-13 12:07:15 -0700

    Create 271-EncodeandDecodeStrings

---
+++ 271-EncodeandDecodeStrings/271-EncodeandDecodeStrings.py
@@ -0,0 +1,18 @@
+# Last updated: 9/13/2025, 12:07:15 PM
+class Codec:
+    def encode(self, strs: List[str]) -> str:
+        """Encodes a list of strings to a single string.
+        """
+        return "\U0001f601".join(strs)
+        
+
+    def decode(self, s: str) -> List[str]:
+        """Decodes a single string to a list of strings.
+        """
+        return s.split("\U0001f601")
+        
+
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(strs))
