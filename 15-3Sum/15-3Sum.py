835f24d49d2c73a895ec27dc8534829899982ad0
dreamldx <dreamldx@gmail.com>
2025-09-19 18:29:27 -0700

    File updated at 9/19/2025, 6:29:27 PM

--- 15-3Sum/15-3Sum.py
+++ 15-3Sum/15-3Sum.py
@@ -1,38 +1,21 @@
-# Last updated: 4/20/2025, 10:34:24 PM
+# Last updated: 9/19/2025, 6:29:27 PM
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        l=0
-        h=len(nums)-1
-        if len(nums)==1:
-            return 0 if target == nums[0] else -1
-        pivot=-1
-        while l<=h:
-            mid=(l+h)//2
-            if mid+1<len(nums) and nums[mid]>nums[mid+1]:
-                pivot=mid
-                break
-            if nums[mid]<nums[0]:
-                h=mid-1
-            else:
-                l=mid+1
+    def isPalindrome(self, s: str) -> bool:
+        p = 0
+        q = len(s) - 1
+        while p<=q:
+            if not s[p].isalnum():
+                p += 1
+                continue
 
-        if target<nums[0]:
-            l=pivot+1
-            h=len(nums)-1
-        else:
-            l=0
-            h=pivot
-        if pivot==-1:
-            l=0
-            h=len(nums)-1
+            if not s[q].isalnum():
+                q -= 1
+                continue
 
-        while l<=h:
-            mid=(l+h)//2
-            if nums[mid]==target:
-                return mid
-                break
-            if nums[mid]<target:
-                l=mid+1
-            else:
-                h=mid-1
-        return -1
+            if s[p].lower() != s[q].lower():
+                return False
+
+            p += 1
+            q -= 1
+
+        return True
